d4289a27918500a9e04c51d59aaa47a8b31b04c8
Leonardo Barbosa Sales <leobarbo@c2r5p1.42sp.org.br>
2023-12-04 15:32:55 -0300

    '#' and '+' working, ft_printf more than 25 lines

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 17:14:39 by leobarbo          #+#    #+#             */
-/*   Updated: 2023/12/02 20:51:25 by leobarbo         ###   ########.fr       */
+/*   Updated: 2023/12/04 15:04:56 by leobarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,14 @@ int	main(void)
 	num2 = 99;
 	num3 = 0;
 
-	ret = ft_printf("%#X", ULONG_MAX);
+	ret = ft_printf("%+++++++++++++++++++++++++++++d", 42);
 	printf("\n\n%d\n\n", ret);
-	ret = printf("%#X", ULONG_MAX);
+	ret = printf("%++++++++++++++++++++++++++++++++d", 42);
 	printf("\n\n%d\n\n", ret);
 
-	ret = ft_printf("%#X", LONG_MIN);
+	ret = ft_printf("%+d", -42);
 	printf("\n\n%d\n\n", ret);
-	ret = printf("%#X", LONG_MIN);
+	ret = printf("%+d", -42);
 	printf("\n\n%d\n\n", ret);
 
 	return (0);
